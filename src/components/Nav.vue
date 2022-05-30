4254d4e14ff845e5848d844cb4a39d46ffc1de63
linkdevk <40721155+kylegl@users.noreply.github.com>
2022-05-30 03:52:59 -0700

    wip

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,18 @@
-<script setup>
-
-</script>
-
 <template>
-    <div class="flex items-start w-full gap-x-8 text-lg bg-bg-a p-4">
-      <router-link to="/" class="text-fg-norm no-underline flex gap-x-2">
-        <div class="i-fa-solid-home text-2xl" />Work Orders
-      </router-link>
-      <router-link to="/directory" class="text-fg-norm no-underline flex gap-x-2">
-        <div class="i-akar-icons-people-group text-2xl" /> Directory
-      </router-link>
-      <router-link to="/jobs" class="text-fg-norm no-underline flex gap-x-2">
-        <div class="i-akar-icons-people-group text-2xl" /> Jobs
-      </router-link>
-    </div>
+  <nav class="flex items-start w-full gap-x-8 text-lg bg-bg-a p-4">
+    <router-link to="/" class="text-fg-norm no-underline flex gap-x-2">
+      <div class="i-fa-solid-home text-2xl" />Work Orders
+    </router-link>
+    <router-link to="/directory" class="text-fg-norm no-underline flex gap-x-2">
+      <div class="i-akar-icons-people-group text-2xl" /> Directory
+    </router-link>
+    <router-link to="/jobs" class="text-fg-norm no-underline flex gap-x-2">
+      <div class="i-akar-icons-people-group text-2xl" /> Jobs
+    </router-link>
+    <button class="icon-btn !outline-none" @click="toggleDark()">
+      <div v-if="isDark" i-carbon-moon />
+      <div v-else i-carbon-sun />
+    </button>
+      {{isDark}}
+  </nav>
 </template>
