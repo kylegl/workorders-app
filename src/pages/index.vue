bf13f7ee60243e43615282658a93006635dc8999
Kylegl <40721155+kylegl@users.noreply.github.com>
2022-05-30 00:19:47 -0700

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+const name = $ref('')
+
+const router = useRouter()
+const go = () => {
+  console.log('button clicked')
+  router.replace('/test')
+}
+</script>
+
+<template>
+  <div>
+    <div i-carbon-campsite text-4xl inline-block />
+    <p>
+      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
+        Vitesse Lite
+      </a>
+    </p>
+    <p>
+      <em text-sm op75>Opinionated Vite Starter Template</em>
+    </p>
+
+    <Counter />
+
+    <div py-4 />
+    {{ `typed ${name}` }}
+
+    <input
+      id="input"
+      v-model="name"
+      placeholder="What's your name?"
+      type="text"
+      autocomplete="false"
+      p="x-4 y-2"
+      w="250px"
+      text="center"
+      bg="transparent"
+      border="~ rounded gray-200 dark:gray-700"
+      outline="none active:none"
+      @keydown.enter="go"
+    >
+
+    <div>
+      <button
+        class="m-3 text-sm btn"
+        @click="go"
+      >
+        Go
+      </button>
+    </div>
+  </div>
+</template>
