6bab2cb16827aa4d23baf67b767f4950674c6002
Kyle Lauber <lauber.kyle@gmail.com>
2022-06-03 03:45:37 -0700

    refactor: use one central store - typing wip

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,6 @@ import 'uno.css'
   if (process.env.NODE_ENV === 'development') {
     const { worker } = await import('~/mocks/browser')
     worker.start({
-      waitUntilReady: true,
       onUnhandledRequest: 'bypass',
     })
   }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 const { client, data, loading, error } = storeToRefs(useMainStore())
-const { query } = useMainStore()
+const { query, getById } = useMainStore()
 
 const result = reactive({ data: undefined })
+
 onMounted(() => {
   result.data = query()
 })
@@ -10,14 +11,38 @@ onMounted(() => {
 const getMock = () => {
   result.data = query()
 }
+
+const test = reactive({})
+const test2 = ref({})
+
+const go = () => {
+  test.value = getById({ id: 1, type: 'jobs', getParsed: true })
+  test2.value = data.value.clients.find(clients => clients.id === test.value.data.client_id)
+}
+
+const changeData = () => {
+
+  test.value.displayValues.client_id.name = "FUCK YOU"
+}
 </script>
 
 <template>
   <div>
     <h1>Home</h1>
-    {{ `result ${result.data}` }}
+    {{ 'getter response' }}
+    {{ test }}
+    <div class="border p-4 rounded bg-b-f">
+      store value
+      {{ test2 }}
+    </div>
     <Button @click="getMock">
       Query
+    </Button>
+    <Button @click="go">
+      Test
+    </Button>
+    <Button @click="changeData">
+      change data
     </Button>
     <div class="">
       {{ `loading ${loading}` }}
