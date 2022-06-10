b8cf06e67cfda8feeee11eab050706af78724194
Kyle Lauber <lauber.kyle@gmail.com>
2022-06-10 01:11:53 -0700

    chore: wip

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 const { client, data, loading, error } = storeToRefs(useMainStore())
-const { query, getById } = useMainStore()
+const { query, getById, getReadableDate } = useMainStore()
 
 const { x, y } = useMouse()
 
@@ -11,15 +11,26 @@ onMounted(() => {
 const getMock = () => {
   query()
 }
-const textValue = $ref('')
-const selectedId = $ref('0e124bdc-b3bc-4edb-85c8-6a1dbb73b562')
+
 const employee_id = $ref('0e124bdc-b3bc-4edb-85c8-6a1dbb73b562')
 
-const test = $ref()
-const test2 = $ref()
+let A = $ref(1)
+
+let B = $ref('')
+
+let workorder = $ref()
+
+const get = () => {
+  const id = '9a249f4e-3289-46fa-9d56-2dadf92d4709'
+  workorder = getById({ id, type: 'workorders' })
+  A = workorder.start_date
+
+  const dateToTimestamp = date => +new Date(date)
+
+  useConvertSyncRefs($$(A), $$(B), unixToDate, dateToTimestamp)
+}
 
 const go = () => {
-
 }
 
 const changeData = () => {
@@ -29,16 +40,14 @@ const changeData = () => {
 <template>
   <div>
     <h1>Home</h1>
-    {{x}}, {{y}}
+    {{ x }}, {{ y }}
     {{ `id emitted: ${employee_id}` }}
-    <Datalist
-      v-model="employee_id"
-      :list="data.employees"
-      :search-keys="['name', 'email']"
-      type="employees"
-      :show-keys="['name', 'position']"
-    />
-
+    <div class="">
+      B: {{ B }}
+    </div>
+    <div class="">
+      A: {{ A }}
+    </div>
     <div class="border p-4 rounded bg-b-f" />
     <div class="border p-4 rounded bg-b-f">
       store value
@@ -47,11 +56,14 @@ const changeData = () => {
     <Button @click="getMock">
       Query
     </Button>
+    <Button @click="get">
+      Get
+    </Button>
     <Button @click="go">
-      Test
+      set A to B
     </Button>
     <Button @click="changeData">
-      change data
+      change B
     </Button>
     <div class="">
       {{ `loading ${loading}` }}
